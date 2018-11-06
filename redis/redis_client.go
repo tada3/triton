cb9a722bc9cef6ac98548efa6d80a470fe414f56
YOSHIHARA TADAFUMI <tadafumi.yoshihara@linecorp.com>
2018-11-06 09:35:02 +0900

    Implemented cache with redis.

---
+++ redis/redis_client.go
@@ -0,0 +1,48 @@
+package redis
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+var client *redis.Client
+
+func init() {
+	var err error
+	client, err = newRedisClient("10.127.34.81:7000")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Get(key string) (string, bool) {
+	v, err := client.Get(key).Result()
+	if err != nil {
+		if err != redis.Nil {
+			// real error
+			fmt.Printf("LOG Redis Get(%s) error: %s", key, err.Error())
+		}
+		return "", false
+	}
+	return v, true
+}
+
+func Set(k, v string, e time.Duration) {
+	err := client.Set(k, v, e).Err()
+	if err != nil {
+		fmt.Printf("LOG Redis Set(%s, %s, %d) error: %s", k, v, e, err.Error())
+	}
+}
+
+func newRedisClient(addr string) (*redis.Client, error) {
+	if addr == "" {
+		return nil, errors.New("addr should not be empty string")
+	}
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		PoolSize: 30,
+	}), nil
+}
--- translation/translation_service.go
+++ translation/translation_service.go
@@ -1,10 +1,17 @@
 package translation
 
-import "github.com/tada3/triton/translation/ms"
+import (
+	"fmt"
+	"time"
+
+	"github.com/tada3/triton/redis"
+	"github.com/tada3/triton/translation/ms"
+)
 
 const (
-	msTranslatorBaseURL = "https://api.cognitive.microsofttranslator.com/"
-	msTranslatorAPIKey  = "78f442ae6aa44e52a221ec70441328c6"
+	msTranslatorBaseURL               = "https://api.cognitive.microsofttranslator.com/"
+	msTranslatorAPIKey                = "78f442ae6aa44e52a221ec70441328c6"
+	cacheTimeout        time.Duration = 24 * time.Hour
 )
 
 var tr Translator
@@ -22,5 +29,29 @@ func init() {
 }
 
 func Translate(w string) (string, error) {
-	return tr.Translate(w)
+	var tw string
+	tw, hit := checkCache(w)
+	if hit {
+		fmt.Printf("LOG Cache Hit %s\n", w)
+		return tw, nil
+	}
+	fmt.Printf("LOG Cache Miss %s\n", w)
+	tw, err := tr.Translate(w)
+	if err != nil {
+		return "", err
+	}
+	setCache(w, tw)
+	return tw, nil
+}
+
+func checkCache(w string) (string, bool) {
+	return redis.Get(getRedisKey(w))
+}
+
+func setCache(w, tw string) {
+	redis.Set(w, tw, cacheTimeout)
+}
+
+func getRedisKey(w string) string {
+	return "triton:translation:" + w
 }
