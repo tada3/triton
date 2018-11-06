58907612637bb3ebfff8da23817f173ff4ca6ea7
YOSHIHARA TADAFUMI <tadafumi.yoshihara@linecorp.com>
2018-11-06 10:31:52 +0900

    more cache.

--- weather/weather_service.go
+++ weather/weather_service.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"time"
+
+	"github.com/tada3/triton/redis"
 	"github.com/tada3/triton/weather/model"
 	"github.com/tada3/triton/weather/owm"
 )
 
 const (
-	OwmBaseURL         string = "https://api.openweathermap.org/data/2.5/"
-	OwmAPIKey          string = "e3fd219fa4ed7117d68e9fcbda3b298e"
-	CurrentWeatherPath string = "weather"
+	OwmBaseURL         string        = "https://api.openweathermap.org/data/2.5/"
+	OwmAPIKey          string        = "e3fd219fa4ed7117d68e9fcbda3b298e"
+	CurrentWeatherPath string        = "weather"
+	cacheTimeout       time.Duration = 30 * time.Minute
 )
 
 var (
@@ -27,28 +31,35 @@ func init() {
 	}
 }
 
-//func GetCurrentWeatherByID(id string) (*model.CurrentWeather, error) {
-//idNum, _ := strconv.ParseInt(id, 10, 64)
-//return owmc.GetCurrentWeatherByID(idNum)
-
-//return owmc.GetCurrentWeatherByID(idNum)
-
-//}
 
 func GetCurrentWeather(cityName string) (*model.CurrentWeather, error) {
+	var cw *model.CurrentWeather
+	cw, ok := checkCache(cityName)
+	if ok {
+		fmt.Printf("LOG Cache Hit %s\n", cityName)
+		return cw, nil
+	}
+
 	cityID, found, err := owm.GetCityID(cityName)
 	if err != nil {
-		// errMsg := "ごめんなさい、システムの調子が良くないようです。しばらくしてからもう一度お試しください。"
-		return nil, err
+		// ignore error hrere
+		fmt.Printf("LOG DB error: %s\n", err.Error())
+		found = false
 	}
+	var err2 error
 	if !found {
 		// use cityEn as it is
-		return owmc.GetCurrentWeatherByName(cityName)
+		cw,err2 =  owmc.GetCurrentWeatherByName(cityName)
 	} else {
 		// use cityID
 		fmt.Printf("cityID: %d\n", cityID)
-		return owmc.GetCurrentWeatherByID(cityID)
+		cw, err2 =  owmc.GetCurrentWeatherByID(cityID)
 	}
+	if err2 != nil {
+		return nil, err2
+	}
+	setCache(cityName, cw)
+	return cw, nil
 }
 
 // DecodeBody decode JSON response body to the specified struct.
@@ -56,4 +67,32 @@ func DecodeBody(resp *http.Response, out interface{}) error {
 	defer resp.Body.Close()
 	decoder := json.NewDecoder(resp.Body)
 	return decoder.Decode(out)
+}
+
+func checkCache(cityName string) (*model.CurrentWeather, bool) {
+	v, ok := redis.Get(getRedisKey(cityName))
+	if !ok {
+		return nil, false
+	}
+	cw := &model.CurrentWeather{}
+	err := json.Unmarshal([]byte(v), cw)
+	if err != nil {
+		fmt.Printf("LOG Unmarshal failed: %s\n", err.Error())
+		return nil, false
+	}
+	return cw, true
+}
+
+func setCache(cityName string, cw *model.CurrentWeather) {
+	b, err := json.Marshal(cw)
+	if err != nil {
+		fmt.Printf("LOG Marshal failed: %s\n", err.Error())
+		return
+	}
+	v := string(b)
+	redis.Set(getRedisKey(cityName), v, cacheTimeout)
+}
+
+func getRedisKey(w string) string {
+	return "triton:weather:" + w
 }
